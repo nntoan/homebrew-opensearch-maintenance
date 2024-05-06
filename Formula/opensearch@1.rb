aa0fd578703eabbb5aa6185ad675e6166be1b2eb
Toan Nguyen <nntoan@users.noreply.github.com>
2024-05-06 10:13:10 +0700

    Downgrade version of opensearch@1 to 1.3.8
    In order to resolve performance issues of Mac M1 ARM64

--- Formula/opensearch@1.rb
+++ Formula/opensearch@1.rb
@@ -1,8 +1,8 @@
 class OpensearchAT1 < Formula
   desc "Open source distributed and RESTful search engine"
   homepage "https://github.com/opensearch-project/OpenSearch"
-  url "https://github.com/opensearch-project/OpenSearch/archive/refs/tags/1.3.16.tar.gz"
-  sha256 "c0b5049a82ad926ea1cbb71d4eb00c12d058c2c0110cc2c6916774f06cad29ab"
+  url "https://github.com/opensearch-project/OpenSearch/archive/1.3.8.tar.gz"
+  sha256 "bb7b131780fea4ceb456ecbd3a534598d3e584886c6880f687a7eb741560e482"
   license "Apache-2.0"
 
   keg_only :versioned_formula
@@ -10,6 +10,8 @@ class OpensearchAT1 < Formula
   depends_on "gradle@6" => :build
   depends_on "openjdk"
 
+  # Backport of https://github.com/opensearch-project/OpenSearch/pull/1668
+  # TODO: Remove when available in release
   on_arm do
     patch :DATA
   end
@@ -102,12 +104,117 @@ class OpensearchAT1 < Formula
 end
 
 __END__
+diff --git a/distribution/archives/build.gradle b/distribution/archives/build.gradle
+index 2c5b91f7e135d0e4a38cf3588bc12a7f28601d39..ac70ee04444c7672981cd31b84b852bdeb17476a 100644
+--- a/distribution/archives/build.gradle
++++ b/distribution/archives/build.gradle
+@@ -95,6 +95,13 @@ distribution_archives {
+     }
+   }
+
++  darwinArm64Tar {
++    archiveClassifier = 'darwin-arm64'
++    content {
++      archiveFiles(modulesFiles('darwin-arm64'), 'tar', 'darwin', 'arm64', true)
++    }
++  }
++
+   noJdkDarwinTar {
+     archiveClassifier = 'no-jdk-darwin-x64'
+     content {
+@@ -102,6 +109,13 @@ distribution_archives {
+     }
+   }
+
++  noJdkDarwinArm64Tar {
++    archiveClassifier = 'no-jdk-darwin-arm64'
++    content {
++      archiveFiles(modulesFiles('darwin-arm64'), 'tar', 'darwin', 'arm64', false)
++    }
++  }
++
+   freebsdTar {
+     archiveClassifier = 'freebsd-x64'
+     content {
+diff --git a/distribution/archives/darwin-arm64-tar/build.gradle b/distribution/archives/darwin-arm64-tar/build.gradle
+new file mode 100644
+index 0000000000000000000000000000000000000000..bb3e3a302c8d6a96a319a1474e964757f5ed3f57
+--- /dev/null
++++ b/distribution/archives/darwin-arm64-tar/build.gradle
+@@ -0,0 +1,13 @@
++/*
++ * SPDX-License-Identifier: Apache-2.0
++ *
++ * The OpenSearch Contributors require contributions made to
++ * this file be licensed under the Apache-2.0 license or a
++ * compatible open source license.
++ *
++ * Modifications Copyright OpenSearch Contributors. See
++ * GitHub history for details.
++ */
++
++// This file is intentionally blank. All configuration of the
++// distribution is done in the parent project.
+diff --git a/distribution/archives/no-jdk-darwin-arm64-tar/build.gradle b/distribution/archives/no-jdk-darwin-arm64-tar/build.gradle
+new file mode 100644
+index 0000000000000000000000000000000000000000..bb3e3a302c8d6a96a319a1474e964757f5ed3f57
+--- /dev/null
++++ b/distribution/archives/no-jdk-darwin-arm64-tar/build.gradle
+@@ -0,0 +1,13 @@
++/*
++ * SPDX-License-Identifier: Apache-2.0
++ *
++ * The OpenSearch Contributors require contributions made to
++ * this file be licensed under the Apache-2.0 license or a
++ * compatible open source license.
++ *
++ * Modifications Copyright OpenSearch Contributors. See
++ * GitHub history for details.
++ */
++
++// This file is intentionally blank. All configuration of the
++// distribution is done in the parent project.
+diff --git a/distribution/build.gradle b/distribution/build.gradle
+index 33232195973f0960f3008f42c6dde84ff410779e..356aaa269e10662872b175a1fcbb5ef30aebc96b 100644
+--- a/distribution/build.gradle
++++ b/distribution/build.gradle
+@@ -280,7 +280,7 @@ configure(subprojects.findAll { ['archives', 'packages'].contains(it.name) }) {
+   // Setup all required JDKs
+   project.jdks {
+     ['darwin', 'linux', 'windows'].each { platform ->
+-      (platform == 'linux' ? ['x64', 'aarch64'] : ['x64']).each { architecture ->
++      (platform == 'linux' || platform == 'darwin' ? ['x64', 'aarch64'] : ['x64']).each { architecture ->
+         "bundled_${platform}_${architecture}" {
+           it.platform = platform
+           it.version = VersionProperties.getBundledJdk(platform)
+@@ -353,7 +353,7 @@ configure(subprojects.findAll { ['archives', 'packages'].contains(it.name) }) {
+           }
+         }
+         def buildModules = buildModulesTaskProvider
+-        List excludePlatforms = ['darwin-x64', 'freebsd-x64', 'linux-x64', 'linux-arm64', 'windows-x64']
++        List excludePlatforms = ['darwin-x64', 'freebsd-x64', 'linux-x64', 'linux-arm64', 'windows-x64', 'darwin-arm64']
+         if (platform != null) {
+           excludePlatforms.remove(excludePlatforms.indexOf(platform))
+         } else {
+diff --git a/settings.gradle b/settings.gradle
+index 3fdc7ec03bf997b7b4ca11992aaa9d5c619376b9..bcf1fd5937668d8856496584b7f22cfbc831c724 100644
+--- a/settings.gradle
++++ b/settings.gradle
+@@ -34,6 +34,8 @@ List projects = [
+   'distribution:archives:windows-zip',
+   'distribution:archives:no-jdk-windows-zip',
+   'distribution:archives:darwin-tar',
++  'distribution:archives:darwin-arm64-tar',
++  'distribution:archives:no-jdk-darwin-arm64-tar',
+   'distribution:archives:no-jdk-darwin-tar',
+   'distribution:archives:freebsd-tar',
+   'distribution:archives:no-jdk-freebsd-tar',
 diff --git a/buildSrc/build.gradle b/buildSrc/build.gradle
-index 3da274ef424..189ffe3490a 100644
+index a13a41309b6..9c925f88641 100644
 --- a/buildSrc/build.gradle
 +++ b/buildSrc/build.gradle
-@@ -127,7 +127,7 @@ dependencies {
-   api 'org.eclipse.jgit:org.eclipse.jgit:6.8.0.202311291450-r'
+@@ -110,7 +110,7 @@ dependencies {
+   api 'com.netflix.nebula:gradle-info-plugin:7.1.3'
    api 'org.apache.rat:apache-rat:0.13'
    api 'commons-io:commons-io:2.7'
 -  api "net.java.dev.jna:jna:5.5.0"
@@ -116,15 +223,16 @@ index 3da274ef424..189ffe3490a 100644
    api 'de.thetaphi:forbiddenapis:3.2'
    api 'com.avast.gradle:gradle-docker-compose-plugin:0.14.12'
 diff --git a/buildSrc/version.properties b/buildSrc/version.properties
-index b3c99919811..6606060e99c 100644
+index 118b43ebdf0..66aebbbd1ce 100644
 --- a/buildSrc/version.properties
 +++ b/buildSrc/version.properties
-@@ -24,7 +24,7 @@ guava             = 32.0.1-jre
- protobuf          = 3.22.3
+@@ -20,7 +20,7 @@ woodstox          = 6.4.0
+ kotlin            = 1.7.10
  
  # when updating the JNA version, also update the version in buildSrc/build.gradle
 -jna               = 5.5.0
 +jna               = 5.11.0
  
- netty             = 4.1.109.Final
- reactor_netty     = 1.0.39
+ netty             = 4.1.86.Final
+ joda              = 2.10.12
+
